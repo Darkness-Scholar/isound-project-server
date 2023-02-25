6f6642dc6df1cbc822d7d584bb67158774f7b3d1
Nguyễn Trọng Đức <ngtrongduc106@gmail.com>
2023-02-25 19:59:18 +0700

    [new] middleware auth token

--- dist/routes/user/user.router.js
+++ dist/routes/user/user.router.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const user_controller_1 = require("./user.controller");
+const auth_middleware_1 = require("../../middlewares/auth.middleware");
 const user = (0, express_1.Router)();
+// user.use(checkAuth)
 user.post("/signin", user_controller_1.default.signin);
 user.post("/signup", user_controller_1.default.signup);
-user.get("/profile", user_controller_1.default.profile);
+user.get("/profile", auth_middleware_1.checkAuth, user_controller_1.default.profile);
 exports.default = user;
--- src/routes/user/user.router.ts
+++ src/routes/user/user.router.ts
@@ -1,10 +1,13 @@
 import { Router } from "express"
 import UserController from "./user.controller"
+import { checkAuth } from "../../middlewares/auth.middleware"
 
 const user = Router()
 
+// user.use(checkAuth)
+
 user.post("/signin", UserController.signin)
 user.post("/signup", UserController.signup)
-user.get("/profile", UserController.profile)
+user.get("/profile", checkAuth, UserController.profile)
 
 export default user 
